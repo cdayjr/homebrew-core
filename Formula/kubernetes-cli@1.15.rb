dc1a4f68ce4f4eb21939456886248da92d043301
Chad Wade Day, Jr <cdayjr@users.noreply.github.com>
2021-04-18 00:34:15 -0700

    Revert "Remove non-building formula"
    This reverts commit 58ff436ec829837c477ed969690276e9bd229c9d.

---
+++ Formula/kubernetes-cli@1.15.rb
@@ -0,0 +1,61 @@
+class KubernetesCliAT115 < Formula
+  desc "Kubernetes command-line interface (1.15)"
+  homepage "https://kubernetes.io/"
+  url "https://github.com/kubernetes/kubernetes.git",
+      tag:      "v1.15.12",
+      revision: "e2a822d9f3c2fdb5c9bfbe64313cf9f657f0a725"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes/kubernetes.git",
+    branch: "release-1.15"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  deprecate! date: "2020-03-25", because: :deprecated_upstream
+
+  depends_on "bash" => :build
+  depends_on "go@1.14" => :build
+
+  uses_from_macos "rsync" => :build
+
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    # Make binary
+    system "make", "WHAT=cmd/kubectl"
+    bin.install "_output/bin/kubectl"
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "bash")
+    (bash_completion/"kubectl").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "zsh")
+    (zsh_completion/"_kubectl").write output
+
+    # Install man pages
+    # Leave this step for the end as this dirties the git tree
+    system "hack/generate-docs.sh"
+    man1.install Dir["docs/man/man1/*.1"]
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+
+    version_output = shell_output("#{bin}/kubectl version --client 2>&1")
+
+    assert_match "GitTreeState:\"clean\"", version_output
+
+    if build.stable?
+      assert_match stable.instance_variable_get(:@resource)
+                         .instance_variable_get(:@specs)[:revision],
+                   version_output
+    end
+  end
+end
