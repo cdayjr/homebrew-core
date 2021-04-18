45e8104a73ab04d9ab9e5ecf5d51e877e4e9f3a1
Chad Wade Day, Jr <cdayjr@users.noreply.github.com>
2021-04-17 23:24:09 -0700

    Add deprecated kubectl formulas

---
+++ Formula/kubernetes-cli@1.18.rb
@@ -0,0 +1,61 @@
+class KubernetesCliAT118 < Formula
+  desc "Kubernetes command-line interface (1.18)"
+  homepage "https://kubernetes.io/"
+  url "https://github.com/kubernetes/kubernetes.git",
+      tag:      "v1.18.18",
+      revision: "6f6ce59dc8fefde25a3ba0ef0047f4ec6662ef24"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes/kubernetes.git",
+    branch: "release-1.18"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  deprecate! date: "2021-04-07", because: :deprecated_upstream
+
+  depends_on "bash" => :build
+  depends_on "go" => :build
+
+  uses_from_macos "rsync" => :build
+
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    # Make binary
+    system "make", "WHAT=cmd/kubectl"
+    bin.install "_output/bin/kubectl"
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "bash")
+    (bash_completion/"kubectl").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "zsh")
+    (zsh_completion/"_kubectl").write output
+
+    # Install man pages
+    # Leave this step for the end as this dirties the git tree
+    system "hack/generate-docs.sh"
+    man1.install Dir["docs/man/man1/*.1"]
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+
+    version_output = shell_output("#{bin}/kubectl version --client 2>&1")
+
+    assert_match "GitTreeState:\"clean\"", version_output
+
+    if build.stable?
+      assert_match stable.instance_variable_get(:@resource)
+                         .instance_variable_get(:@specs)[:revision],
+                   version_output
+    end
+  end
+end
